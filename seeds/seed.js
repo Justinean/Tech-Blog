8ea9974a6b461abfbcb82737c56d4ffe35e67b89
Justin Hoch <justinhoch22@gmail.com>
2021-07-16 15:54:52 -0500

    Added seeds to experiment with and fixed small bugs in the models

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,1 +1,32 @@
+const sequelize = require('../config/connection');
+const { User, Blog, Comment } = require('../models');
 
+const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  let i = 0;
+  for (const blog of blogData) {
+    const newBlog = await Blog.create({
+      ...blog,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+    console.log(commentData[i])
+    await Comment.create({
+      content: commentData[i].content,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      blog_id: newBlog.id
+    })
+    i++
+  }
+  process.exit(0);
+};
+
+seedDatabase();
