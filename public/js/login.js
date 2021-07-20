e5bada0e6aa65424d90342dc9ed283a97e36d2a8
Justin Hoch <justinhoch22@gmail.com>
2021-07-20 16:25:38 -0500

    Made password and username requirements

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,28 @@
 let loginBtn = $("#loginBtn");
 
 signUp = async event => {
-    let object = $(event.target)
-    let username = object.parent().siblings().eq(1).val();
-    let password = object.parent().siblings().eq(3).val();
+  let object = $(event.target)
+  let username = object.parent().siblings().eq(1).val();
+  let password = object.parent().siblings().eq(3).val();
+  let regEx = /^[0-9a-zA-Z]+$/;
+  let err = "Invalid username or password!"
+  checkValue = (regEx, val, err) => {
+    if (regEx.test(val)) {
+      return true;
+    }
+    else {
+      alert(err);
+      return false;
+    }
+  }
+  let validUser = checkValue(regEx, username, err)
+  regEx = /^[0-9a-zA-Z,.!@#$%^&*()_+-=]+$/
+  let validPass = checkValue(regEx, password, err)
+  if (validPass && validUser) {
     response = await fetch('/api/users/login', {
-        method: "POST",
-        headers: {"content-type": "application/json"},
-        body: JSON.stringify({username, password})
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ username, password })
     })
     data = await response.json();
     if (data.message[0] === "Y") {
@@ -15,6 +30,7 @@ signUp = async event => {
     } else {
       alert(data.message);
     }
+  }
 }
 
 loginBtn.on("click", signUp);
--- public/js/signup.js
+++ public/js/signup.js
@@ -4,18 +4,36 @@ signUp = async event => {
     let object = $(event.target)
     let username = object.parent().siblings().eq(1).val();
     let password = object.parent().siblings().eq(3).val();
-    response = await fetch('/api/users', {
-        method: "POST",
-        headers: {"content-type": "application/json"},
-        body: JSON.stringify({username, password})
-    })
-    data = await response.json();
-    if (response.status === 200) {
-        window.location.href = "/";
-    } else if (response.status === 400) {
-        alert("Username already exists!");
-    } else {
-        alert("Could not create account!");
+    let regEx = /^[0-9a-zA-Z]+$/;
+    let err = "Username must be alphanumeric"
+    checkValue = (regEx, val, err) => {
+        if (regEx.test(val)) {
+        return true;
+        }
+        else {
+        alert(err);
+        return false;
+        }
+    }
+    let validUser = checkValue(regEx, username, err)
+    regEx = /^[0-9a-zA-Z/,.!@#$%^&*()_+-=]+$/
+    console.log(regEx)
+    err = 'Password must be alphanumeric with only these symbols "/.!@#$%^&*()_+-="'
+    let validPass = checkValue(regEx, password, err)
+    if (validPass && validUser) {
+        response = await fetch('/api/users', {
+            method: "POST",
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify({username, password})
+        })
+        data = await response.json();
+        if (response.status === 200) {
+            window.location.href = "/";
+        } else if (response.status === 400) {
+            alert("Username already exists!");
+        } else {
+            alert("Could not create account!");
+        }
     }
 }
 
